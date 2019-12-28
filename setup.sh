88fdb5a00a7db1b562c149233f49ae42d7694a38
jj <dd>
2019-12-28 03:12:33 +0100

    First import.

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -x
+
+# Make sure we have pulled in and updated any modules
+
+echo $(pwd)
+
+git submodule init
+git submodule update
+
+# what directories should be installable by all users including the root user
+base=(
+	bash
+	git
+	Xresources
+	gtk
+	alacritty
+	sway
+)
+
+# run the stow command for the passed in directory ($2) in location ($1)
+
+stowit() {
+	usr=$1
+	app=$2
+	# -v verbose
+	# -R recurseive
+	# -t target
+	stow -v -R -t ${usr} ${app}
+}
+
+echo ""
+echo "Stowing apps for user: $(whoami)"
+
+cd configs
+
+for app in ${base[@]}; do
+	stowit $HOME $app
+done 
+
+echo ""
+echo "###### ALL DONE"
